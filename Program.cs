c3d5bcdd66a6658dea29d93b354389df2148aa2a
Maxim Palchikov <palchmx@gmail.com>
2025-07-03 08:26:52 +0300

    Add serilog

--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSerilog(
+    (services, lc) => lc
+        .ReadFrom.Configuration(builder.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext()
+        .WriteTo.Console());
 
 var app = builder.Build();
 
+app.UseSerilogRequestLogging();
 app.UseHttpsRedirection();
 
 app.MapGet("/", () =>
